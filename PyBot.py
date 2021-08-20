ae2bbd8d78db3402b15a18ec00b33bdfba3123a4
Gabriel L. Pereira <ogabrielpereira@pm.me>
2021-08-20 00:10:45 -0300

    Update PyBot.py

--- PyBot.py
+++ PyBot.py
@@ -3,15 +3,16 @@ import discord
 class MyClient(discord.Client):
     
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print(f'Logged on as {self.user}')
+        
 
     async def on_message(self, message):
-
+        
         if message.author == self.user:
             return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        if message.content == 'Hi':
+            await message.channel.send('Hi, how are you?')
 
 client = MyClient()
 client.run('Put your bot token here')
